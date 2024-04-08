cf84c25ab38bc4d4badd37d3f53c9359984469d9
dereck lhw <dereck.lamhonwah@gmail.com>
2024-04-08 20:27:44 +0400

    feat: add loader and websocket event open, close and error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-screen">
     <Navbar />
-    <main class="flex-grow">
+    <main v-if="!loading" class="flex-grow">
       <div class="flex flex-col h-full bg-slate-100">
         <div class="flex-grow basis-1/3 border-t-2 border-slate-950">
           <h1 class="text-lg px-4 py-3 font-medium">Overview</h1>
@@ -32,6 +32,10 @@
         </div>
       </div>
     </main>
+    <div v-else class="flex flex-col justify-center items-center h-screen">
+      <ProgressSpinner />
+      <h2 class="mt-8 text-2xl">Loading...</h2>
+    </div>
   </div>
 </template>
 
@@ -41,8 +45,9 @@ import ZoomableTimeSeries from "./components/ZoomableTimeSeries.vue";
 import Candlestick from "./components/Candlestick.vue";
 import Pie from "./components/Pie.vue";
 import Dropdown from "primevue/dropdown";
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import { SYMBOL } from "./types";
+import ProgressSpinner from "primevue/progressspinner";
 
 const symbols: SYMBOL[] = [
   { name: "BTC" },
@@ -52,6 +57,40 @@ const symbols: SYMBOL[] = [
   { name: "DOGE" },
 ];
 const selectedSymbol = ref<SYMBOL>(symbols[0]);
+const latestSentiments = ref([]);
+const historicData = ref([]);
+const predictions = ref([]);
+const loading = ref(true);
+
+const websocketUrl =
+  "wss://sq12h10asg.execute-api.us-east-1.amazonaws.com/prod/";
+
+const socket = new WebSocket(websocketUrl);
+
+onMounted(() => {
+  // Connection opened
+  socket.addEventListener("open", () => {
+    console.log("The connection has been opened successfully.");
+  });
+
+  // Connection closed
+  socket.addEventListener("close", () => {
+    console.log("The connection has been closed successfully.");
+  });
+
+  // Listen for possible errors
+  socket.addEventListener("error", (event) => {
+    console.log("WebSocket error: ", event);
+  });
+
+  setTimeout(() => {
+    loading.value = false;
+  }, 2000);
+});
+
+onUnmounted(() => {
+  socket.close();
+});
 </script>
 
 <style scoped></style>
