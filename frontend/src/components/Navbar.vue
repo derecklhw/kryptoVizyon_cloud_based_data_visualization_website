f1f51bbf847f2120c6ba3b2b82c1c502f9a3a029
dereck lhw <dereck.lamhonwah@gmail.com>
2024-04-08 10:19:56 +0400

    feat: current frontend layout

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template >
+    <div class="py-3 px-4">
+        <p class="text-xl font-bold">KryptoVizyon</p>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style >
+    
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import VueApexCharts from "vue3-apexcharts";
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(VueApexCharts).mount('#app')
